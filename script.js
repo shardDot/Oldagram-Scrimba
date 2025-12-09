a2d12540e0527a6528850899e3b1fbb5ae8187c7
shardDot <oshards@proton.me>
2025-12-09 11:55:16 -0300

    Final Commit

--- script.js
+++ script.js
@@ -28,10 +28,40 @@ const posts = [
     }
 ]
 
+const feed = document.querySelector(".feed")
+
+render(posts)
 
 function render(posts) {
-    
+    postsHTML = ""
     for(let i = 0; i < posts.length; i++) {
-        
+        postsHTML += `
+            <div class="card">
+                <div class="card-nav">
+                    <img src="src/${posts[i].avatar}" alt="${posts[i].name} avatar" class="user-avatar">
+                    <div class="post-info">
+                        <p id="poster-name" class="bold">${posts[i].name}</p>
+                        <p id="poster-location">${posts[i].location}</p>
+                    </div>
+                </div>
+
+                <img id="post" src="src/${posts[i].post}" alt="">
+
+                <div class="card-footer">
+                    <div class="interact">
+                        <img src="src/images/icon-heart.png" alt="">
+                        <img src="src/images/icon-comment.png" alt="">
+                        <img src="src/images/icon-dm.png" alt="">
+                    </div>
+
+                    <p id="likes-amount" class="bold">${posts[i].likes} likes</p>
+                    <p id="comments">
+                    <strong>${posts[i].username}</strong> ${posts[i].comment}
+                    </p>
+                </div>
+            </div>
+        `
+
+        feed.innerHTML = postsHTML
     }
 }
